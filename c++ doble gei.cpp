983f893c1c459bc0a1379244b904431a96229bf3
JavierFrancoRamírez <43232622+xXxbYkoNdeLuKanoRxXx@users.noreply.github.com>
2018-11-26 19:59:04 +0100

    Add files via upload

---
+++ c++ doble gei.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1;
+	cout<< "digame un numero turro: ";
+	cin>> n1;
+	if (n1%2==0)
+		cout<< "el numero es par pelotudo: ";
+	if (n1%2!=0)
+		cout<< "el numero es impar, sos doble gei ";
+	
+	return 0;
+}
